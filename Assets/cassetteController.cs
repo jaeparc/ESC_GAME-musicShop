fa4adfdf6f0bf30f9ef322baa1a44348a75f1e56
jaeparc <tgarnon45@gmail.com>
2023-11-23 12:06:35 +0100

    cassetteController proto done

--- Assets/cassetteController.cs
+++ Assets/cassetteController.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interactable.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class cassetteController : MonoBehaviour
 {
     public GameObject[] sockets = new GameObject[4];
+    bool[] socketLocked = new bool[4];
 
-    void lock(int id){
-        //sockets[id].GetComponent<XRSocketInteractor>().GetOldestGetOldestInteractableSelected().transform.gameObject.GetComponent<XRGrabInteractable>().InteractionLayerMask
+    public void lockCoin(int id){
+        sockets[id].GetComponent<XRSocketInteractor>().GetOldestInteractableSelected().transform.gameObject.GetComponent<MeshCollider>().enabled = false;
+        socketLocked[id] = true;
+        testDone();
+    }
+
+    void testDone(){
+        bool done = true;
+        for(int i = 0; i < socketLocked.Length; i++){
+            if(!socketLocked[i])
+                done = false;
+        }
+        if(done){
+            for(int j = 0; j < sockets.Length; j++)
+                sockets[j].GetComponent<Renderer>().material.SetColor("_Color",Color.green);
+        }
     }
 }
